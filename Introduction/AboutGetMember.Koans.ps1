d5e96d8a32c63e627d9af7e78a1292208049495d
HiddenWisent <asdowa447@gmail.com>
2024-09-16 12:26:25 +0200

    finished on variables

--- Introduction/AboutGetMember.Koans.ps1
+++ Introduction/AboutGetMember.Koans.ps1
@@ -60,24 +60,24 @@ Describe "Get Member" {
         #>
         $BecauseString = "property '{0}' should be present in output from {1}"
 
-        $Cmdlet1 = '____'
-        $PropertyName = '____'
+        $Cmdlet1 = 'Get-Date'
+        $PropertyName = 'Date'
 
         $Reason = $BecauseString -f $PropertyName, $cmdlet1
         & (Get-Command -Name $Cmdlet1) |
             Get-Member -MemberType Property -Name $PropertyName |
             Should -Not -BeNullOrEmpty -Because $Reason
 
-        $Cmdlet2 = '____'
-        $PropertyName = '____'
+        $Cmdlet2 = 'Get-Help'
+        $PropertyName = 'Length'
 
         $Reason = $BecauseString -f $PropertyName, $cmdlet2
         & (Get-Command -Name $Cmdlet2) |
             Get-Member -MemberType Property -Name $PropertyName |
             Should -Not -BeNullOrEmpty -Because $Reason
 
-        $Cmdlet3 = '____'
-        $PropertyName = '____'
+        $Cmdlet3 = 'Get-ChildItem'
+        $PropertyName = 'Extension'
 
         $Reason = $BecauseString -f $PropertyName, $cmdlet3
         & (Get-Command -Name $Cmdlet3) |
@@ -104,22 +104,22 @@ Describe "Get Member" {
         #>
         $BecauseString = "method '{0}' should be present in output from {1}"
 
-        $Cmdlet1 = '____'
-        $MethodName = '____'
+        $Cmdlet1 = 'Get-Help'
+        $MethodName = 'Normalize'
         $Reason = $BecauseString -f $MethodName, $cmdlet1
         & (Get-Command -Name $Cmdlet1) |
             Get-Member -MemberType Method -Name $MethodName |
             Should -Not -BeNullOrEmpty -Because $Reason
 
-        $Cmdlet2 = '____'
-        $MethodName = '____'
+        $Cmdlet2 = 'Get-Random'
+        $MethodName = 'ToBoolean'
         $Reason = $BecauseString -f $MethodName, $cmdlet2
         & (Get-Command -Name $Cmdlet2) |
             Get-Member -MemberType Method -Name $MethodName |
             Should -Not -BeNullOrEmpty -Because $Reason
 
-        $Cmdlet3 = '____'
-        $MethodName = '____'
+        $Cmdlet3 = 'Get-TimeZone'
+        $MethodName = 'IsDaylightSavingTime'
         $Reason = $BecauseString -f $MethodName, $cmdlet3
         & (Get-Command -Name $Cmdlet3) |
             Get-Member -MemberType Method -Name $MethodName |
